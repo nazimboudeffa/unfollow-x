aeb6e3c11044f0c7a4a6e83ec91838001f9cf0dd
Nazim Boudeffa <boudeffa@gmail.com>
2025-02-07 00:34:25 +0100

    added fart sound

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,11 @@ export default function Home() {
     console.log("Submitting form");
   };
 
+  const playAudio = () => {
+    const audio = new Audio("/assets/short-fart-185140.mp3");
+    audio.play();
+  };
+
   return (
     <>
     <div className="bg-indigo-600 px-4 py-3 text-white">
@@ -19,34 +24,33 @@ export default function Home() {
     </div>
     <NavBar />
     <div className="flex flex-col items-center justify-center min-h-screen">
-    <h1 className="text-3xl sm:text-4xl text-center font-bold">
-      Organise Massive Unfollow Campaigns
-    </h1>
-    <div className="flex flex-col items-center justify-center mt-5 py-2">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center sm:items-start">
-        <label htmlFor="input" className="text-lg sm:text-xl">
-          Explain why, how and who you want to unfollow
-        </label>
-        <textarea
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          id="input"
-          placeholder="I want to massively unfollow Elon Musk because his latest tweets are too controversial."
-          className="border border-gray-300 p-2 rounded"
-          rows={4} 
-          cols={50}
-          disabled={true}
-        >
-        </textarea>
-        <button
-          type="submit"
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-indigo-600 text-background gap-2 hover:bg-indigo-700 text-sm sm:text-white h-10 sm:h-12 px-4 sm:px-5"
-          disabled={true}
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+      <h1 className="text-3xl sm:text-4xl text-center font-bold">
+        Organise Massive Unfollow Campaigns
+      </h1>
+      <div className="flex flex-col items-center justify-center py-2">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center sm:items-start">
+          <label htmlFor="input" className="text-lg sm:text-xl">
+            Explain why, how and who you want to unfollow
+          </label>
+          <textarea
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            id="input"
+            placeholder="I want to massively unfollow Elon Musk because his latest tweets are too controversial."
+            className="border border-gray-300 p-2 rounded"
+            rows={4} 
+            cols={50}
+          >
+          </textarea>
+          <button
+            type="submit"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-indigo-600 text-background gap-2 hover:bg-indigo-700 text-sm sm:text-white h-10 sm:h-12 px-4 sm:px-5"
+            onClick={playAudio}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
     </>
   );
