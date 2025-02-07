009c9fa3e09d038048d37da23f1f7a55aa1f2958
Nazim Boudeffa <boudeffa@gmail.com>
2025-02-07 18:19:07 +0100

    added campaign

--- src/app/campaigns/page.tsx
+++ src/app/campaigns/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Alert from "../components/Alert";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
@@ -7,10 +8,27 @@ export default function Campaigns() {
         <>
         <Alert />
         <NavBar />
-        <div className="flex items-center justify-center min-h-screen">
-            There is no campaign actually
+        <div className="m-auto my-10 max-w-5xl space-y-10 px-3">
+            <h1 className="font-bold">Unfollow X is a joke to celebrate 21 years of Facebook</h1>
+            <p className="mt-3">But if you really want to develop it, please edit the corresponding file on GitHub</p>
+            <ul className="mt-3">
+                <li>
+                    <Link className="hover:underline cursor-pointer" href="https://x.com/elonmusk">
+                        Click and unfollow <span className="text-red-500">Elon Musk</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link className="hover:underline cursor-pointer" href="https://x.com/CNEWS">
+                        Click and unfollow <span className="text-red-500">CNews</span>
+                    </Link>
+                </li>
+                <li>
+                    <Link className="hover:underline cursor-pointer" href="https://x.com/ZemmourEric">
+                        Click and unfollow <span className="text-red-500">Eric Zemmour</span>
+                    </Link>
+                </li>
+            </ul>
         </div>
-        <Footer />
         </>
     );
 }
