da61e4ad6c5caea147fb3f308aab1e47eaf2011b
Nazim Boudeffa <boudeffa@gmail.com>
2025-02-08 11:21:14 +0100

    changed some text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,6 +10,7 @@ export default function About() {
         <h1 className="font-bold text-2xl">About</h1>
         <p className="mt-3">I just want to explain you something.</p>
         <p className="mt-3">Leave the influencers in their world and stop following them.</p>
+        <p className="mt-3">You must have in mind that if you follow somebody, you feed his ego</p>
         <h1 className="font-bold mt-5">DISCLAIMER: Unfollow X is a joke to celebrate 21 years of Facebook</h1>
         <p className="mt-3">If you really want to develop it, please edit the corresponding file on GitHub</p>
         <p className="mt-3">You can also contribute by submitting a pull request</p>
